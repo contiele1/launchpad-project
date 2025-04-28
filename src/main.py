51c19df181e5be0c5135267fc7c147dc3d6bb33e
contiele1 <conti.daniele.13@gmail.com>
2025-04-28 23:59:58 +0100

    add send_to_queue func

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 import requests
 import pprint
+import boto3
+import json
+from dotenv import load_dotenv
+import os
+
 
 
 def call_api(api_key: str, content:str = None, date:str = None):
@@ -22,3 +27,32 @@ def call_api(api_key: str, content:str = None, date:str = None):
                          for result in results['response']['results']]
 
     return selected_results
+
+
+
+def send_to_queue(messages: list):
+            
+        sqs = boto3.client("sqs")
+
+        queue_url = "https://sqs.eu-west-2.amazonaws.com/522585361178/FirstQueue"
+
+        for message in messages:
+                message = json.dumps(message)
+                response = sqs.send_message(
+                    QueueUrl=queue_url,
+                    MessageBody=message
+                    ) 
+                print(response['MessageId'])
+
+
+
+
+if __name__ == "__main__":
+        
+        load_dotenv()
+        api_key = os.environ["API-KEY"]
+
+        messages = call_api(api_key, "chess")
+        
+        send_to_queue(messages)
+        
