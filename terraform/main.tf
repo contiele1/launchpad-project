d8e811f5292a31fa385f0686200b203e1610d8d9
contiele1 <conti.daniele.13@gmail.com>
2025-05-01 12:32:09 +0100

    add s3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    bucket = "guardian-content-remote-state-bucket"
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
 provider "aws" {
     region = "eu-west-2"
 }
