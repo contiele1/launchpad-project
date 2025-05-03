1d1044afa48514dca92c8e40ed4a2b682ec1c92a
Daniele Conti <conti.daniele.13@gmail.com>
2025-05-04 01:42:45 +0200

    fix python code

--- local.py
+++ local.py
@@ -1,10 +1,38 @@
+from datetime import datetime
+import time
 from src.main import lambda_handler
 from dotenv import load_dotenv
 import os
 
-
-load_dotenv()
-
-api_key = os.environ["API-KEY"]
-
-lambda_handler({"api_key": api_key}, {})
+if __name__ == "__main__":
+    while True:
+        content = input("Please insert a search term: ")
+        if content:
+            break
+    while True:
+        date = input("If you want, insert a starting date for your search (dd-mm-yyyy), otherwise simply press Enter: ")
+        def is_dd_mm_yyyy_format(date_str: str) -> bool:
+            try:
+                datetime.strptime(date_str, "%d-%m-%Y")
+                return True
+            except ValueError:
+                return False
+        if is_dd_mm_yyyy_format(date):
+            date_obj = datetime.strptime(date, "%d-%m-%Y")
+            date = date_obj.strftime("%Y-%m-%d")
+            break
+        if date == "":
+            date = None
+            break
+        else:
+            print("Wrong date format.")
+            time.sleep(1)
+           
+    load_dotenv()
+    api_key = os.environ["API-KEY"]
+    event = {
+        "content": content,
+        "api_key": api_key,
+        "date": date
+    }
+    lambda_handler(event, {})
--- src/main.py
+++ src/main.py
@@ -1,34 +1,65 @@
+from typing import Any, Optional
 import requests
 import boto3
 import json
 
+GUARDIANAPIS_URL = "https://content.guardianapis.com/search"
 
-def call_api(api_key: str, content: str = None, date: str = None):
+def call_api(api_key: str, content: str, date: str = None, debug: bool = True
+             ) -> Optional[list[dict[str, Any]]]:
+    
+    """Make a call to the Guardian search API.
 
-    URL = "https://content.guardianapis.com/search"
+    Args:
+        api_key (str): API key for guardianpapis.com
+        content (str): Keyword to search
+        date (str): Date to start search from
+        debug (bool, optional): Print debug info. Defaults to True.
+
+    Returns:
+        Optional[list[dict[str, Any]]]: A list of relevant searches, or None
+        if something went wrong.
+    """
 
     params = {"q": content, "from-date": date, "order-by": "newest", "api-key": api_key}
+    response = requests.get(url=GUARDIANAPIS_URL, params=params)
+    print(response.url)
 
-    response = requests.get(url=URL, params=params)
+    if response.status_code != 200:
+        print(f"Failed to get response: {response.status_code=}")
+        return None
+    
+    if debug:
+        print(response)
+    
+    selected_results = process_response_json(response.json())
+    
+    if not selected_results:
+        print(f"No results found for keyword {content}")
+        return None
+    
+    return selected_results
 
-    print(response)
-
-    results = response.json()
-    selected_results = [
+def process_response_json(response_json: dict[str, Any]) -> list[dict[str, Any]]:
+    if "response" not in response_json:
+        print("No 'response' in returned JSON")
+        return []
+    if "results" not in response_json["response"]:
+        print("No 'results' in returned JSON")
+        return []
+    results = response_json["response"]["results"]
+    return [
         {
             "webPublicationDate": result["webPublicationDate"],
             "webTitle": result["webTitle"],
             "webUrl": result["webUrl"],
         }
-        for result in results["response"]["results"]
+        for result in results
     ]
-    return selected_results
 
 
 def send_to_queue(messages: list):
-
     sqs = boto3.client("sqs", region_name="eu-west-2")
-
     queue_url = sqs.get_queue_url(QueueName="guardian_content")["QueueUrl"]
 
     for message in messages:
@@ -38,8 +69,16 @@ def send_to_queue(messages: list):
 
 
 def lambda_handler(event, context):
-    api_key = event.get("api_key")
-    content = event.get("content")
+    def required_contents(key: str) -> str:
+        if value := event.get(key):
+            return value
+        else:
+            raise ValueError(f"A required key is missing from event: {key}")
+    api_key = required_contents("api_key")
+    content = required_contents("content")
     date = event.get("date")
-    messages = call_api(api_key, content, date)
-    send_to_queue(messages)
+
+    if messages := call_api(api_key, content, date):
+        send_to_queue(messages)
+    else:
+        print("No messages to send to queue")
